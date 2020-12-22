d9d53c692934d77242a8c3f16c0d60da94b0fa36
Anders Lundsgård <anders@utvecklare.eu>
2020-12-22 06:56:13 +0100

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "core-vpc"
+#   }
+# }
+
+resource "aws_s3_bucket" "some_buckets" {
+    for_each = {
+        food = "public-read"
+        cash = "private"
+    }
+    bucket = "lundsgard-course-demo-${each.key}"
+    acl = each.value
+}
+
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World!"
+}
