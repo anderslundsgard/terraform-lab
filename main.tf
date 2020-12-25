fad9881bfb86e35f536fe5b20fdcac67549ed284
Anders Lundsgård <anders@utvecklare.eu>
2020-12-25 06:41:43 +0100

    Move terraform block to own file.

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-terraform {
-  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
-  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.13.x code.
-  required_version = ">= 0.12.26"
-}
-
 data "aws_caller_identity" "current" {
 
 }
@@ -21,7 +14,7 @@ resource "aws_s3_bucket" "some_buckets" {
     food = "public-read"
     cash = "private"
   }
-  bucket = "lundsgard-course-demo-${each.key}"
+  bucket = "lundsgard-intro-course-demo-${each.key}"
   acl    = each.value
 }
 
